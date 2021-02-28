06786b1247d0275a72cdfb63fe5efff30f8f3914
nofprog <k.jin3217@gmail.com>
2021-02-28 11:37:32 +0900

    AutoLayout, 国旗の表示

--- HWS_Project2/ViewController.swift
+++ HWS_Project2/ViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        //Aeetts内の画像の名前と同じ
+        
+        button1.layer.borderWidth = 1.0
+        button2.layer.borderWidth = 1.0
+        button3.layer.borderWidth = 1.0
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+                
+        
+        askQuestion()
+    
+    }
+    
+    func askQuestion(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
